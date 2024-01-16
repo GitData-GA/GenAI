752af8b1663fb8cf9a380a176fccd1dcef79e33a
Lee <120439693+lygitdata@users.noreply.github.com>
2024-01-16 09:36:26 -0500

    Add files via upload

--- R/src/R/GenAI_utils.R
+++ R/src/R/GenAI_utils.R
@@ -31,11 +31,14 @@ moderation.openai = function(model.parameter, prompt) {
 
 #' @noRd
 image.to.data.uri = function(image.path) {
-  response = httr::GET(image.path)
-  image.data = base64enc::base64encode(httr::content(response, type = "raw"))
-  parts = strsplit(image.path, "\\.")[[1]]
-  file.extension = tolower(tail(parts, 1))
-  return(c(file.extension, image.data))
+  image.data = ""
+  if (grepl("^https?://", tolower(image.path))) {
+    response = httr::GET(image.path)
+    image.data = base64enc::base64encode(httr::content(response, type = "raw"))
+  } else {
+    image.data = base64enc::base64encode(readBin(image.path, "raw", file.info(image.path)$size))
+  }
+  return(c(tools::file_ext(image.path), image.data))
 }
 
 #' @noRd
