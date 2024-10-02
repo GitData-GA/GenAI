a2a906192d6b66e0df89590ef906645db82a598c
Lee <120439693+lygitdata@users.noreply.github.com>
2024-10-02 15:16:49 +0200

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,7 +2,7 @@
 export default defineNitroConfig({
     preset: 'vercel_edge',
     vercel: {
-        regions: ["cle1", "iad1", "sfo1", "pdx1", "hnd1", "icn1", "sin1", "kix1", "arn1", "cdg1", "dub1", "fra1", "lhr1", "syd1"]
+        regions: ["cle1", "iad1", "sfo1", "pdx1", "hnd1", "icn1", "sin1", "kix1", "arn1", "cdg1", "dub1", "fra1", "lhr1", "syd1", "gru1", "cpt1"]
     },
     routeRules: {
         '/': {
