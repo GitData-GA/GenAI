8bb1f2ebed880829afc3639d7539a7e2c0ae5694
admin-gd <127959916+admin-gd@users.noreply.github.com>
2024-12-08 21:11:42 +0100

    Update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -16,6 +16,9 @@ export default defineNitroConfig({
         },
         '/moonshot/**': {
             proxy: 'https://api.moonshot.cn/**'
+        },
+        '/login/**': {
+            proxy: "https://login.microsoftonline.com/**"
         }
     }
 });
