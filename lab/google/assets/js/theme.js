47765df7a9f12833ed1e38ec6b84f9e79dc56e65
Lee <120439693+lygitdata@users.noreply.github.com>
2025-09-22 23:06:58 +0200

    Add files via upload

--- lab/google/assets/js/theme.js
+++ lab/google/assets/js/theme.js
@@ -697,6 +697,13 @@ const callGeminiApi = async (text, images) => {
     const API_URL = `${baseUrl}v1beta/models/${geminiModel}:streamGenerateContent?key=${geminiApiKey}`;
 
     const payload = {
+        system_instruction: {
+          parts: [
+            {
+              text: "You are a friendly and helpful assistant. Ensure your answers are complete, unless the user requests a more concise approach. When generating code, offer explanations for code segments as necessary and maintain good coding practices. When presented with inquiries seeking information, provide answers that reflect a deep understanding of the field, guaranteeing their correctness. For any non-english queries, respond in the same language as the prompt unless otherwise specified by the user. For prompts involving reasoning, provide a clear explanation of each step in the reasoning process before presenting the final answer."
+            }
+          ]
+        },
         contents: chatHistory
     };
 
